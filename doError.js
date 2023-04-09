883d3c8ec59176be1ebc942ac1ccaaca55eebe76
Alexsandra <aleksamahaeva@yandex.ru>
2023-04-09 22:25:03 +0300

    14work

--- doError.js
+++ doError.js
@@ -1,5 +1,5 @@
-module.exports.doError = (err, req, res, next) => {
-  const { statusCode = 500 } = err
-  const message = statusCode === 500 ? 'Произошла ошибка' : err.message
-  res.status(statusCode).send({ message })
-}
+module.exports.doError = (err, req, res) => {
+  const { statusCode = 500 } = err;
+  const message = statusCode === 500 ? 'Произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
+};
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken')
-const AuthorizError = require('../errors/authorizError')
+const jwt = require('jsonwebtoken');
+const AuthorizError = require('../errors/authorizError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.userToken
-  if (!token) throw new AuthorizError('Необходима авторизация')
-  let payload
+  const token = req.cookies.userToken;
+  if (!token) throw new AuthorizError('Необходима авторизация');
+  let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key')
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    throw new AuthorizError('Необходима авторизация')
+    throw new AuthorizError('Необходима авторизация');
   }
 
-  req.user = payload
-  next()
-}
+  req.user = payload;
+  next();
+};
