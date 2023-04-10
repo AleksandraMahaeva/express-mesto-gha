0036b25d9cabba0f809d821261aaa6e4fd88059b
AleksandraMahaeva <95657556+AleksandraMahaeva@users.noreply.github.com>
2023-04-10 12:29:58 +0300

    Delete .eslintrc.js

--- doError.js
+++ doError.js
@@ -1,5 +1,6 @@
-module.exports.doError = (err, req, res) => {
+module.exports.doError = (err, req, res, next) => {
   const { statusCode = 500 } = err;
   const message = statusCode === 500 ? 'Произошла ошибка' : err.message;
   res.status(statusCode).send({ message });
+  next(err);
 };
